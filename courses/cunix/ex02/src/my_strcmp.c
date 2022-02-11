4dac15a81812b5120098662dc92d86071826f5b7
Ostap <ostap.omelchuk@gmail.com>
2022-02-11 15:00:40 +0200

    solve ex02

---
+++ courses/cunix/ex02/src/my_strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+unsigned int my_strcmp(char *str1, char *str2) {
+    int diff = 0; 
+    if(str1 != str2) {
+        int min_length = strlen(str1);
+
+        if ((int)strlen(str2) < min_length) {
+            min_length =  strlen(str2);
+        }
+        printf("length = %d \n", min_length);
+
+        for(int i = 0; i < min_length; i++) {
+            if ((int)str1[i] > (int)str2[i]) {
+                return 1;
+            }
+            else if ((int)str1[i] < (int)str2[i])
+            {
+                return -1;
+            }   
+        }
+    }
+    return diff;
+}
